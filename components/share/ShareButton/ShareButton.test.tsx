ce0b2c105f89a31ec2659781a06b693d07bec83c
Hiro <hiroshi.takeuchi111@gmail.com>
2021-11-13 07:13:57 +0000

    added useModalEnabled hook to ShareButton.tsx

--- components/share/ShareButton/ShareButton.test.tsx
+++ components/share/ShareButton/ShareButton.test.tsx
@@ -1,9 +1,25 @@
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 
 import ShareButton from "./ShareButton";
+
+// mock useModalEnabled
+const mockEnable = jest.fn();
+jest.mock("../../../hooks/linkModalHooks", () => ({
+  useModalEnabled: () => mockEnable,
+}));
+
+beforeEach(() => {
+  mockEnable.mockClear();
+});
 
 it("should render share button", () => {
   expect.assertions(1);
   const { getByText } = render(<ShareButton />);
   expect(getByText("Share")).toBeTruthy();
+});
+
+it("should invoke onclick callback", () => {
+  const { getByText } = render(<ShareButton />);
+  fireEvent.click(getByText("Share"));
+  expect(mockEnable).toHaveBeenCalledTimes(1);
 });
--- components/share/ShareButton/ShareButton.tsx
+++ components/share/ShareButton/ShareButton.tsx
@@ -2,11 +2,14 @@ import { Fab } from "@mui/material";
 import ShareIcon from "@mui/icons-material/Share";
 
 import styles from "./ShareButton.module.css";
+import { useModalEnabled } from "../../../hooks/linkModalHooks";
 
 export default function ShareButton() {
+  const enableModal = useModalEnabled();
+
   return (
     <div className={styles.container}>
-      <Fab color="secondary" variant="extended">
+      <Fab color="secondary" variant="extended" onClick={enableModal}>
         <ShareIcon sx={{ mr: 1 }} />
         Share
       </Fab>
